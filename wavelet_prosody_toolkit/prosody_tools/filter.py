0ec5d6163bf69207f7e96a3fc26edeed4bc29841
Sebastien Le Maguer <slemaguer@coli.uni-saarland.de>
2018-01-10 12:27:00 +0100

    Add filter module documentation

--- wavelet_prosody_toolkit/prosody_tools/filter.py
+++ wavelet_prosody_toolkit/prosody_tools/filter.py
@@ -1,10 +1,33 @@
-# copied from scipy cookbook
-#http://scipy-cookbook.readthedocs.io/items/ButterworthBandpass.html
+# -*- coding: utf-8 -*-
+"""Butter filter utilities
+
+This module contains butter filter help functions copied from http://scipy-cookbook.readthedocs.io/items/ButterworthBandpass.html
+"""
 
 from scipy.signal import butter, lfilter
 
 
 def butter_bandpass(lowcut, highcut, fs, order=5):
+    """Generate the butter bandpass filter
+
+    For more details see scipy.signal.butter documentation
+
+    Parameters
+    ----------
+    lowcut: int
+        The low cut value
+    highcut: type
+        description
+    fs: int
+        Signal sample rate
+    order: int
+        Order of the butter fiter
+
+    Returns
+    -------
+    Tuple
+    	Numerator (`b`) and denominator (`a`) polynomials of the IIR filter
+    """
     nyq = 0.5 * fs
     low = lowcut / nyq
     if highcut >=nyq*0.95:
@@ -15,8 +38,25 @@ def butter_bandpass(lowcut, highcut, fs, order=5):
 
 
 def butter_bandpass_filter(data, lowcut, highcut, fs, order=5):
+    """Filter signal data using a butter filter type
+
+    For more details see scipy.signal.butter and scipy.signal.lfilter documentation
+
+    Parameters
+    ----------
+    data: array_like
+        An N-dimensional input array.
+    lowcut: int
+        The lowcut filtering value.
+    highcut: type
+        The highcut filtering value.
+    fs: int
+        The signal sample rate.
+    order: int
+        The order of the butter filter.
+
+    """
     b, a = butter_bandpass(lowcut, highcut, fs, order=order)
     y = lfilter(b, a, data)
 
     return y
-
