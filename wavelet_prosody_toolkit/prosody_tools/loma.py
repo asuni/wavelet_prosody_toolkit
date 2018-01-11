be4eb4791544978cbc2e5b8ae053da502008457c
Sebastien Le Maguer <slemaguer@coli.uni-saarland.de>
2018-01-11 13:44:33 +0100

    Baseline documentation for loma module

--- wavelet_prosody_toolkit/prosody_tools/loma.py
+++ wavelet_prosody_toolkit/prosody_tools/loma.py
@@ -1,12 +1,34 @@
+"""Module which provides Line Of Maximum Amplitude (loma) related routines
+
+"""
 import numpy as np
 from operator import itemgetter
+
+from wavelet_prosody_toolkit.prosody_tools import misc
 
 # Logging
 import logging
 logger = logging.getLogger(__name__)
 
+
 def save_analyses(fname, labels, prominences, boundaries, frame_rate=200, with_header=False):
     """Save analysis into a csv formatted this way
+
+    Parameters
+    ----------
+    fname: string
+        The output csv filename
+    labels: list of tuple (float, float, string)
+        List of labels which are lists of 3 elements [start, end, description]
+    prominences: type
+        description
+    boundaries: type
+        description
+    frame_rate: int
+        The speech frame rate
+    with_header: boolean
+        Write the header (True) or not (False) [default: False]
+
     """
     import os.path
 
@@ -18,12 +40,12 @@ def save_analyses(fname, labels, prominences, boundaries, frame_rate=200, with_h
     lines = []
     for i in range(0, len(labels)):
         lines.append(("%s" %(os.path.splitext(os.path.basename(fname))[0] ),
-                     "%.3f" %(float(labels[i][0]/frame_rate)),
+                      "%.3f" %(float(labels[i][0]/frame_rate)),
                       "%.3f" %(float(labels[i][1]/frame_rate)),
                       labels[i][2],
                       #"%.3f" %(float(prominences[i][0]/frame_rate)),
                       "%.3f" %(prominences[i][1]),
-                     "%.3f" %(boundaries[i][1])))
+                      "%.3f" %(boundaries[i][1])))
 
     logger.debug("Saving %s with following content:" % fname)
     if with_header:
@@ -38,8 +60,16 @@ def save_analyses(fname, labels, prominences, boundaries, frame_rate=200, with_h
         for i in range(0,len(lines)):
             prom_f.write(u'\t'.join(lines[i])+u"\n")
 
-def simplify(loma):
 
+def simplify(loma):
+    """?
+
+    Parameters
+    ----------
+    loma: type
+        description
+
+    """
     simplified = []
     for l in loma:
         # align loma to it's position in the middle of the line
@@ -48,8 +78,20 @@ def simplify(loma):
         simplified.append((pos,strength))
     return simplified
 
-def get_prominences(pos_loma, labels, rate=1):
 
+def get_prominences(pos_loma, labels, rate=1):
+    """?
+
+    Parameters
+    ----------
+    pos_loma: list of ?
+        Positive loma values
+    labels: list of tuple (float, float, string)
+        List of labels which are lists of 3 elements [start, end, description]
+    rate: int
+        ?
+
+    """
     max_word_loma = []
     loma = simplify(pos_loma)
     for (st, end, unit) in labels:
@@ -67,11 +109,19 @@ def get_prominences(pos_loma, labels, rate=1):
     return max_word_loma
 
 
-
-
-
-# get strongest lines of minimum amplitude between adjacent words' max lines
 def get_boundaries(max_word_loma,boundary_loma, labels):
+    """get strongest lines of minimum amplitude between adjacent words' max lines
+
+    Parameters
+    ----------
+    max_word_loma: type
+        description
+    boundary_loma: type
+        description
+    labels: type
+        description
+
+    """
     boundary_loma = simplify(boundary_loma)
     max_boundary_loma = []
     st = 0
@@ -95,23 +145,24 @@ def get_boundaries(max_word_loma,boundary_loma, labels):
     return max_boundary_loma
 
 
-
-
-def get_peaks(params, threshold=-10):
-    indices = []
-    threshold = 0.001
-
-    threshold = -100
-    zc = np.where(np.diff(np.sign(np.diff(params))))[0]
-    indices = (np.diff(np.sign(np.diff(params))) < 0).nonzero()[0] +1
-
-    peaks = params[indices]
-    return np.array([peaks[peaks>threshold], indices[peaks>threshold]])
-
 def _get_parent(child_index, parent_diff, parent_indices):
+    """Private function to find the parent of the given child peak. At child peak index, follow the
+    slope of parent scale upwards to find parent
 
-    # at child peak index, follow the slope of parent scale upwards to find parent
+    Parameters
+    ----------
+    child_index: int
+        Index of the current child peak
+    parent_diff: list of ?
+        ?
+    parent_indices: list of int ?
+        Indices of available parents
 
+    Returns
+    _______
+    int
+    	The parent index or None if there is no parent
+    """
     for i in range(0, len(parent_indices)):
         if (parent_indices[i] > child_index):
             if (parent_diff[int(child_index)] > 0):
@@ -126,20 +177,35 @@ def _get_parent(child_index, parent_diff, parent_indices):
         return parent_indices[-1]
     return None
 
-
-# lines of maximum amplitude
-
-# note: change this so that one level is done in one chunk, not one parent.
-
 def get_loma(wavelet_matrix, scales, min_scale, max_scale):
     """Get the Line Of Maximum Amplitude (loma)
+
+    Parameters
+    ----------
+    wavelet_matrix: matrix of float
+        The wavelet matrix
+    scales: list of int
+        The list of scales
+    min_scale: int
+        The minimum scale
+    max_scale: int
+        The maximum scale
+
+    Returns
+    -------
+    list of tuples
+    	?
+
+    Note
+    ----
+    change this so that one level is done in one chunk, not one parent.
     """
     psize = 100.0
     min_peak = -10000.0 # minimum peak amplitude to consider. NOTE:this has no meaning unless scales normalized
     max_dist = 10 # how far in time to look for parent peaks. NOTE: frame rate and scale dependent, FIXME: how dependent?
 
     # get peaks from the first scale
-    (peaks,indices) = get_peaks(wavelet_matrix[min_scale],min_peak)
+    (peaks,indices) = misc.get_peaks(wavelet_matrix[min_scale],min_peak)
 
     loma=dict()
     root=dict()
@@ -153,7 +219,7 @@ def get_loma(wavelet_matrix, scales, min_scale, max_scale):
         max_dist = np.sqrt(scales[i])*4
 
 	# find peaks in the parent scale
-        (p_peaks,p_indices) = get_peaks(wavelet_matrix[i], min_peak)
+        (p_peaks,p_indices) = misc.get_peaks(wavelet_matrix[i], min_peak)
 
         parents = dict(zip(p_indices, p_peaks))
 
@@ -196,6 +262,18 @@ def get_loma(wavelet_matrix, scales, min_scale, max_scale):
 
 
 def plot_loma(loma, fig, color='black'):
+    """Plot the line of maximum amplitudes (loma)
+
+    Parameters
+    ----------
+    loma: list of tuple (float, float, int, ?)
+        the loma values
+    fig: figure
+        the figure where the loma are going to be plotted in
+    color: string
+        the color name/code
+
+    """
     for elt in loma:
         for child in elt:
             i = child[2]
