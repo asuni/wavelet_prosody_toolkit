dd31b734d643893c69ee211eb2525be7307b5cf6
Sebastien Le Maguer <lemagues@tcd.ie>
2024-04-29 11:34:14 +0300

    fix: test scripts and configuration

--- test/run_test.sh
+++ test/run_test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Check default script
-python3 wavelet_prosody_toolkit/cwt_analysis_synthesis.py -v samples/01l_fact_0001.wav 01l_fact_0001.cwt
+cwt_analysis_synthesis -v samples/01l_fact_0001.wav 01l_fact_0001.cwt
 diff 01l_fact_0001.cwt test/resources/01l_fact_0001.cwt
 ret=$?
 if [ $ret != 0 ]; then
@@ -9,7 +9,7 @@ if [ $ret != 0 ]; then
 fi
 
 # Check prosody labeller
-python3 wavelet_prosody_toolkit/prosody_labeller.py -v -o test_libri -c wavelet_prosody_toolkit/configs/libritts.yaml samples/libritts
+prosody_labeller -v -o test_libri -c wavelet_prosody_toolkit/configs/libritts.yaml samples/libritts
 python3 test/diff_num.py  test_libri test/resources/libritts
 #diff -r test_libri test/resources/libritts
 ret=$?
